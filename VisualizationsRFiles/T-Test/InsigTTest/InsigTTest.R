5ba219473cb25cea83b393fef446bc8d01a620f5
Kamal Abdelrahman <kamalabdel97@gmail.com>
2020-03-29 02:44:38 -0400

    Insig/Sig TTest Folders
    Created folders for significant and insignificant T-Test

--- VisualizationsRFiles/T-Test/InsigTTest/InsigTTest.R
+++ VisualizationsRFiles/T-Test/InsigTTest/InsigTTest.R
@@ -1,8 +1,10 @@
+set.seed(1)
+
 library(ggplot2)
 
 
 StoreA <- rnorm(365, 5000.21, 332) # Creates 365 samples of occurences, with a mean of 5000.21 and standard deviation of 332
-StoreB <- rnorm(365, 1000.12, 332) # Creates 365 samples of occurences, with a mean of 1000.12 and standard deviation of 332
+StoreB <- rnorm(365, 4855.12, 332) # Creates 365 samples of occurences, with a mean of 1000.12 and standard deviation of 332
 
 Revenue <- c(StoreA, StoreB) #Combines values into one column. All 365 values of Store A will be combined along with all 365 values of Store B
 
@@ -10,7 +12,7 @@ Store <- rep(c("Store A", "Store B"), each = 365) #Creates of column of Store A 
 
 Sales <- data.frame(Store, Revenue) #Creates a dataframe of the Revenue and their corresponding store 
 
-TTestplot <- ggplot(Sales, aes(Store, Revenue, label = Revenue, fill = Store)) + 
+IngsigTTestplot <- ggplot(Sales, aes(Store, Revenue, label = Revenue, fill = Store)) + 
 	stat_summary(fun.y = "mean", geom = "bar") + #
 	stat_summary(aes(label=round(..y..,2)), 
 		fun.y=mean, geom="text", size=3.5, vjust = -0.5) + #Plots the text of the average value for each level
@@ -23,13 +25,13 @@ TTestplot <- ggplot(Sales, aes(Store, Revenue, label = Revenue, fill = Store)) +
 		)
    
 
-ggsave("/Users/kamal/Documents/GitHub/StatsSheet/VisualizationsRFiles/TTestPlot.jpeg", #Pathway to save plot to 
-	TTestplot #Indicates to save the TTestplot 
+ggsave("InsigTTestPlot.jpeg", #Pathway to save plot to 
+	IngsigTTestplot #Indicates to save the TTestplot 
 )
 
 write.csv(Sales, #Indicates to write the Sales dataframe
-	"/Users/kamal/Documents/GitHub/StatsSheet/VisualizationsRFiles/TTestData.csv" #Indicates the filepath to save the data to
-	)
+	"IngsigTTestData.csv" #Indicates the filepath to save the data to
+)
 
 
 
